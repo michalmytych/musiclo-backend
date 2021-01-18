13d900f11f32481789fea24d542f223f311e918a
mike <michamytych@gmail.com>
2021-01-18 11:43:22 +0100

    init commit on infinite-scroll feature branch

--- frontend/src/temporary.js
+++ frontend/src/temporary.js
@@ -13,8 +13,6 @@ export var SONGS = [
         "instrumentalness"  : "1.09e-05",
         "liveness"          : 0.356,
         "valence"           : 0.503,
-        "time_signature"    : 4.0,
-        "year"              : 1999,
         "release_date"      : "1999-11-02",
         "spotify_link"      : "https://open.spotify.com/embed/track/7lmeHLHBe4nmXzuXc0HDjk"
     },
@@ -32,8 +30,6 @@ export var SONGS = [
         "instrumentalness"  : "1.12e-05",
         "liveness"          : 0.16,
         "valence"           : 0.303,
-        "time_signature"    : 3.0,
-        "year"              : 2004,
         "release_date"      : "2004-01-12",
         "spotify_link"      : "https://open.spotify.com/embed/track/2SwgVZn9S4NGueAaEAryf1"
     },
@@ -51,8 +47,6 @@ export var SONGS = [
         "instrumentalness"  : "1.49e-05",
         "liveness"          : 0.236,
         "valence"           : 0.113,
-        "time_signature"    : 7.0,
-        "year"              : 1987,
         "release_date"      : "1987-10-07",
         "spotify_link"      : "https://open.spotify.com/embed/track/6Uz62Rd9XadpjT8Zebgwq2"
     },
@@ -70,10 +64,59 @@ export var SONGS = [
         "instrumentalness"  : "2.09e-05",
         "liveness"          : 0.156,
         "valence"           : 0.003,
-        "time_signature"    : 4.0,
-        "year"              : 2017,
         "release_date"      : "2017-03-09"
         // "spotify_link"      : "https://open.spotify.com/embed/track/76zFGcQrel57hYKVLseQDN"
+    },
+    {
+        "id"                : "3kZZKnzRJyVIDMasL8cRQO",
+        "name"              : "Eight of Nine",
+        "albums_id"         : [ "6prPsuR3shSCqcxPB84dkg" ],
+        "artist_ids"        : ['LC8PXXgk7YtAIobtjSdNi'],
+        "explicit"          : false,
+        "danceability"      : 0.272,
+        "energy"            : 0.128,
+        "key"               : 8,
+        "mode"              : 1,
+        "acousticness"      : 0.2111,
+        "instrumentalness"  : 0.212,
+        "liveness"          : 0.123,
+        "valence"           : 0.023,
+        "release_date"      : "2017-03-09",
+        "spotify_link"      : "https://open.spotify.com/embed/track/3kZZKnzRJyVIDMasL8cRQO"
+    },
+    {
+        "id"                : "5Y1k7siw6hZG6lfPkGe734",
+        "name"              : "One Track Mind",
+        "albums_id"         : [ "31TRqoVBTQi0lzlPLtvINn" ],
+        "artist_ids"        : ['2BTZIqw0ntH9MvilQ3ewNY'],
+        "explicit"          : false,
+        "danceability"      : 0.23,
+        "energy"            : 0.1234,
+        "key"               : 8,
+        "mode"              : 1,
+        "acousticness"      : 0.523,
+        "instrumentalness"  : 0.23,
+        "liveness"          : 0.432,
+        "valence"           : 0.123,
+        "release_date"      : "2017-03-09",
+        "spotify_link"      : "https://open.spotify.com/embed/track/5Y1k7siw6hZG6lfPkGe734"
+    },
+    {
+        "id"                : "4WJ51qD9MgEEIXmdJYNIms",
+        "name"              : "Camera One",
+        "albums_id"         : [ "4r86aGhGL1jTJKMZ81Puof" ],
+        "artist_ids"        : ['0cHOhL2s162yp5ERTNAZdf'],
+        "explicit"          : true,
+        "danceability"      : 0.34,
+        "energy"            : 0.63,
+        "key"               : 8,
+        "mode"              : 1,
+        "acousticness"      : 0.345,
+        "instrumentalness"  : 0.234,
+        "liveness"          : 0.63,
+        "valence"           : 0.25,
+        "release_date"      : "2017-03-09",
+        "spotify_link"      : "https://open.spotify.com/embed/track/4WJ51qD9MgEEIXmdJYNIms"
     }
 ];
 
@@ -149,32 +192,46 @@ TABELE:
 tracks {
     "id"                : varchar(22) PRIMARY KEY
     "name"              : varchar(128)
+
     "albums_id"         : varchar(22) FOREIGN KEY
     "artist_ids"        : varchar(22) FOREIGN KEY
-    "explicit"          : BOOL
+    
     "danceability"      : FLOAT (3)
     "energy"            : FLOAT (3)
-    "key"               : CHOICE INT 1 - 11
-    "mode"              : BOOL
     "acousticness"      : FLOAT (3)
     "instrumentalness"  : FLOAT (3)
     "liveness"          : FLOAT (3)
     "valence"           : FLOAT (3)
+    
+    "explicit"          : BOOL
+    "key"               : CHOICE INT 1 - 11
+    "mode"              : BOOL
     "release_date"      : DATE
+
+    "created_at"        : DATETIME
 }
 
 albums {
     "id"                : varchar(22) PRIMARY KEY
     "name"              : varchar(128)
+
     "artist_ids"        : varchar(22) FOREIGN KEY
+
     "explicit"          : BOOL
     "release_date"      : DATE
+
+    "created_at"        : DATETIME
 }
 
 artists {
     "id"                : varchar(22) PRIMARY KEY
     "name"              : varchar(128)
+
     "albums_id"         : varchar(22) FOREIGN KEY
+
+    "description"       : varchar(512)
+
+    "created_at"        : DATETIME
 }
 
 
