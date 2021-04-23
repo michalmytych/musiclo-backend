d8527827580df04563c3b0ed5a5b72f817d765b1
mike <michamytych@gmail.com>
2021-04-23 23:39:01 +0200

    add updateSongs & updateArtists at Album model

--- api_laravel_version/example-app/app/Models/Album.php
+++ api_laravel_version/example-app/app/Models/Album.php
@@ -30,16 +30,45 @@ class Album extends Model
 
     protected $primaryKey = 'id';
 
+    /**
+     * Returns album relations with Songs
+     * @return HasMany
+     */
     public function songs(): HasMany
     {
         return $this->hasMany(Song::class)
             ->select('album_id', 'name', 'id');
     }
 
+    /**
+     * Returns album relations with Aritsts
+     * @return BelongsToMany
+     */
     public function artists(): belongsToMany
     {
         return $this->belongsToMany(Artist::class)->
             select('album_id', 'name', 'artists.id');
+    }
+
+    /**
+     * Updates album relations with Songs
+     * @param array $songsIds
+     */
+    public function updateSongs(array $songsIds) : void
+    {
+        $songsToSave = Song::all()->whereIn('id', $songsIds);
+        $this->songs()->delete();
+        $this->songs()->saveMany($songsToSave);
+    }
+
+    /**
+     * Updates album relations with Artists
+     * @param array $artistsIs
+     */
+    public function updateArtists(array $artistsIs) : void
+    {
+        $artistsToSync = Artist::all()->whereIn('id', $artistsIs);
+        $this->artists()->sync($artistsToSync);
     }
 }
 
