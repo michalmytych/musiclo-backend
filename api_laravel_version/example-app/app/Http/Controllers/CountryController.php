05aadf26ed4d9ea66cfaf4d4740c8a2fb01f3b35
mike <michamytych@gmail.com>
2021-04-23 22:22:21 +0200

    add error handling at index action,Country Controlller

--- api_laravel_version/example-app/app/Http/Controllers/CountryController.php
+++ api_laravel_version/example-app/app/Http/Controllers/CountryController.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Country;
+use App\Models\Country;;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class CountryController extends Controller
 {
     /**
      * Returns collection of country objects
      *
-     * @return Country[]|\Illuminate\Database\Eloquent\Collection
+     * @return Country[]|\Illuminate\Database\Eloquent\Collection|JsonResponse
      */
     public function index()
     {
-        return Country::all();
+        try {
+            return Country::all();
+        }
+        catch (Exception $e) {
+            Log::error('Error while fetching data from database',
+                [ 'exception' => $e]
+            );
+            return new JsonResponse([
+                'status'  => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => 'Internal server error'
+            ]);
+        }
     }
+
 }
